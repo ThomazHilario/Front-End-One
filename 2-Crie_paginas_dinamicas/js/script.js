f7fb1fe285e42a26f130714cdb0bc81b0e6fa243
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-18 19:17:52 -0300

    feat: Script.js
    - Adicionando soms aos botões com javaScript.

--- 2-Crie_paginas_dinamicas/js/script.js
+++ 2-Crie_paginas_dinamicas/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+function playSound(src){
+    const audio = new Audio(src);
+    audio.play()
+}
+
+document.querySelectorAll('.tecla').forEach(element => {
+    const idSom = `som_${element.className.split(' ')[1]}`
+
+    element.addEventListener('click', () => {
+        playSound(document.getElementById(idSom).src)
+    })
+})
