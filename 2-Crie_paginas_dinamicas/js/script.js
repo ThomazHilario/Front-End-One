0d5b33c6cb4aa12ada69237f0122532bf173882e
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-18 19:42:12 -0300

    Feat: Script.js
    - Adicionando evento de key para reproduzir os sons.

--- 2-Crie_paginas_dinamicas/js/script.js
+++ 2-Crie_paginas_dinamicas/js/script.js
@@ -10,3 +10,21 @@ document.querySelectorAll('.tecla').forEach(element => {
         playSound(document.getElementById(idSom).src)
     })
 })
+
+const somObject = {
+    'q':'som_tecla_pom',
+    'w':'som_tecla_clap',
+    'e':'som_tecla_tim',
+    'a':'som_tecla_puff',
+    's':'som_tecla_splash',
+    'd':'som_tecla_toim',
+    'z':'som_tecla_psh',
+    'x':'som_tecla_tic',
+    'c':'som_tecla_tom'
+}
+
+window.addEventListener('keydown', (e) => {
+    if(somObject[e.key]){
+        playSound(document.getElementById(somObject[e.key]).src)
+    }
+})
