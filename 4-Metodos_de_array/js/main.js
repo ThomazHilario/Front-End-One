7c23bee2494adadf8459b04518a555b75138bdf8
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-26 18:37:45 -0300

    Valor total dos livros apenas para a secao disponivel

--- 4-Metodos_de_array/js/main.js
+++ 4-Metodos_de_array/js/main.js
@@ -1,7 +1,7 @@
 // buttons
 const buttons = document.querySelectorAll('.btn')
 // section 
-const livros_disponiveis = document.querySelector('.livros__disponiveis')
+const livros_disponiveis = document.querySelector('#valor_total_livros_disponiveis')
 
 // livros
 let livros = []
@@ -12,6 +12,8 @@ buttons.forEach(button => {
         if(button.value !== ''){
             const booksFilter = livros.filter(livro => livro.categoria.toLowerCase() === button.value)
 
+            livros_disponiveis.innerHTML = ''
+
             loadBooks(booksFilter)
         } else{
             if(button.classList[1].includes('ordenacao')){
@@ -19,11 +21,20 @@ buttons.forEach(button => {
 
                 livrosOrder.sort((a,b) => a.preco - b.preco)
 
+                livros_disponiveis.innerHTML = ''
+
                 loadBooks(livrosOrder)
             }
 
             if(button.classList[1].includes('disponiveis')){
                 const livrosDisponiveis = livros.filter(livro => livro.quantidade > 0)
+
+                // valor total dos livros
+                const totalBookValues = livrosDisponiveis.map((item) => item.preco - (item.preco * 0.3)).reduce((acc, item) => acc += item).toFixed(2)
+
+                livros_disponiveis.innerHTML = `<div class="livros__disponiveis">
+                <p>Todos os livros disponíveis por R$ <span id="valor">${totalBookValues.replace('.',',')}</span></p>
+                </div>`
 
                 loadBooks(livrosDisponiveis)
 
@@ -73,10 +84,6 @@ function loadBooks(array){
 
     // Valores em string do total de livros
     const dataString = dataModified.join()
-
-    // valor total dos livros
-    const totalBookValues = array.map((item) => item.preco - (item.preco * 0.3)).reduce((acc, item) => acc += item).toFixed(2)
     
     document.getElementById('livros').innerHTML = dataString
-    livros_disponiveis.innerHTML = `<p>Todos os livros disponíveis por R$ <span id="valor">${totalBookValues.replace('.',',')}</span></p>`
 }
