cc15685a55cf70baaede87678ba89f899d31ff6f
Thomaz Alves <thomazhilario5@gmail.com>
2024-05-06 16:24:23 -0300

    feat: Criando function para enviar os videos para a api

--- 6-Criando_requisicoes/js/connectionApi.js
+++ 6-Criando_requisicoes/js/connectionApi.js
@@ -8,3 +8,19 @@ export async function loadVideos(){
         console.log(error)
     }
 }
+
+async function sendvideo(titulo, url, imagem){
+    try {
+        const response = await fetch('http://localhost:3000/videos', {
+            method:'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify()
+        })
+
+        return response.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
